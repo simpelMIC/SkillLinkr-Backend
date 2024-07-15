45e2eb5e31760ac16b8a2a29a50448f242cbfb8d
TheKeineAhnung <fireball73@gmx.de>
2024-07-15 20:21:38 +0200

    🎨 removed testing return

--- src/database/seeders/permissions.ts
+++ src/database/seeders/permissions.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@database/prisma.js';
 
 export default async function () {
-  const t = await prisma.permission.upsert({
+  await prisma.permission.upsert({
     create: {
       name: 'user.other.modify',
       description: 'Permission to modify other user and release user'
@@ -14,5 +14,4 @@ export default async function () {
       id: true
     }
   });
-  return t;
 }
